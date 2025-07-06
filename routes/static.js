f2e6cf4ff48708507591b1e4e13e49e27978d619
sarbutey <sarbuteyd@gmail.com>
2025-07-06 00:13:08 +0000

    siteee

--- routes/static.js
+++ routes/static.js
@@ -6,12 +6,6 @@ router.get("/", (req, res) => {
   res.render("index");
 });
 
-// Static file serving
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "/public/css"));
-router.use("/js", express.static(__dirname + "/public/js"));
-router.use("/images", express.static(__dirname + "/public/images"));
-
 module.exports = router;
 
 
